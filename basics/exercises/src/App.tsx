732cc8bacb0016eeb34d8760908e4f80e6b10166
rohan <rohan.vazirani@gmail.com>
2026-06-20 11:20:05 -0700

    fix: replace Vite boilerplate App.tsx with Day 3 version

--- basics/exercises/src/App.tsx
+++ basics/exercises/src/App.tsx
@@ -1,47 +1,11 @@
-import './App.css';
+import { Counter } from './components/Counter'
 
-import viteLogo from '/vite.svg';
-import {useState} from 'react';
-
-import reactLogo from './assets/react.svg';
-import {Counter} from './components/Counter';
-
-function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <>
-            <div>
-                <a href='https://vite.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
-            <p>
-                <h1>React Day 3</h1>
-                <Counter label='Apples' />
-                <Counter label='Oranges' startAt={5} />
-            </p>
-        </>
-    );
+export default function App() {
+  return (
+    <>
+      <h1>React Day 3</h1>
+      <Counter label="Apples" />
+      <Counter label="Oranges" startAt={5} />
+    </>
+  )
 }
-
-export default App;
